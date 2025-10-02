e197e0ec0ffdb6ec60a3856f1e50c244e4944dc0
Troreen <att53.53.53@gmail.com>
2025-10-02 10:39:59 +0200

    added comments to make itemSpec table easier to read

--- DiabloUppgift/DiabloUppgift/InventoryTypes.cpp
+++ DiabloUppgift/DiabloUppgift/InventoryTypes.cpp
@@ -2,14 +2,15 @@
 
 const ItemSpec gItemSpecs[] =
 {
-    { ItemId::ShortSword,     "Short Sword",     ItemType::Weapon,      1, 5.0f,  true,  EquipmentSlot::MainHand, 4, 0,  2,  0,  0, 0, 0 },
-    { ItemId::BattleAxe,      "Battle Axe",      ItemType::Weapon,      1, 7.0f,  true,  EquipmentSlot::MainHand, 6, 0,  4, -1,  1, 0, 0 },
-    { ItemId::LongBow,        "Long Bow",        ItemType::Weapon,      1, 40.0f,  true,  EquipmentSlot::MainHand, 5, 0,  0,  3,  0, 0, 0 },
-    { ItemId::LeatherArmor,   "Leather Armor",   ItemType::Armor,       1, 6.0f,  true,  EquipmentSlot::Chest,    0, 2,  0,  0,  1, 0, 0 },
-    { ItemId::ChainmailArmor, "Chainmail Armor", ItemType::Armor,       1, 20.0f, true,  EquipmentSlot::Chest,    0, 3,  0, -1,  2, 0, 0 },
-    { ItemId::PlateArmor,     "Plate Armor",     ItemType::Armor,       1, 15.0f, true,  EquipmentSlot::Chest,    0, 5,  0, -2,  3, 0, 0 },
-    { ItemId::HealthPotion,   "Health Potion",   ItemType::HealthPotion,5, 0.5f,  false, EquipmentSlot::MainHand, 0, 0,  0,  0,  0, 20, 0 },
-    { ItemId::FuryEnchant,    "Fury Scroll",     ItemType::Enchantment, 1, 0.0f,  false, EquipmentSlot::MainHand, 2, 0,  2,  1,  0, 0, 5 }
+    // ID                     Name               Type                     MaxStack   Weight  HasSlot   Slot                      Atk Def Str Dex Phy Heal Dur
+    { ItemId::ShortSword,     "Short Sword",     ItemType::Weapon,        1,         5.0f,   true,     EquipmentSlot::MainHand,  4,  0,  2,  0,  0,   0,  0 },
+    { ItemId::BattleAxe,      "Battle Axe",      ItemType::Weapon,        1,         7.0f,   true,     EquipmentSlot::MainHand,  6,  0,  4, -1,  1,   0,  0 },
+    { ItemId::LongBow,        "Long Bow",        ItemType::Weapon,        1,        40.0f,   true,     EquipmentSlot::MainHand,  5,  0,  0,  3,  0,   0,  0 },
+    { ItemId::LeatherArmor,   "Leather Armor",   ItemType::Armor,         1,         6.0f,   true,     EquipmentSlot::Chest,     0,  2,  0,  0,  1,   0,  0 },
+    { ItemId::ChainmailArmor, "Chainmail Armor", ItemType::Armor,         1,        20.0f,   true,     EquipmentSlot::Chest,     0,  3,  0, -1,  2,   0,  0 },
+    { ItemId::PlateArmor,     "Plate Armor",     ItemType::Armor,         1,        15.0f,   true,     EquipmentSlot::Chest,     0,  5,  0, -2,  3,   0,  0 },
+    { ItemId::HealthPotion,   "Health Potion",   ItemType::HealthPotion,  5,        0.5f,   false,     EquipmentSlot::MainHand,  0,  0,  0,  0,  0,  20,  0 },
+    { ItemId::FuryEnchant,    "Fury Scroll",     ItemType::Enchantment,   1,        0.0f,   false,     EquipmentSlot::MainHand,  2,  0,  2,  1,  0,   0,  5 }
 };
 
 const size_t gItemSpecCount = sizeof(gItemSpecs) / sizeof(gItemSpecs[0]);
